5a2a43b6a81062197e8fd5506054bab66d690d8f
JordanMieger <miegerjordan2000@gmail.com>
2023-10-17 16:47:32 +0200

    Fin quête CSS React

---
+++ src/components/Card.jsx
@@ -0,0 +1,18 @@
+
+
+function Card() {
+    return( 
+        <div className="card">
+            <div className="left-card">
+                <img src="https://ncsmusic.s3.eu-west-1.amazonaws.com/tracks/000/000/936/1000x0/royalty-1619082030-xBgqGZWLw9.jpg" alt="Royalty music picture"/>
+            </div>
+            <div className="right-card">
+                <h1>Royalty</h1>
+                <h2>Ezgod <br />2021</h2>
+                <a href="https://www.youtube.com/watch?v=lW9ep22YmlM&ab_channel=Egzod" target="blank">Click to listen</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
